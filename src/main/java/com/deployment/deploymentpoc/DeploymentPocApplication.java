78aad951a8b0cb8502a9aeb1d7e2c4cb3d1b229a
Shivakrishna Nandamuri <shivakrishna.nandamuri@meijer.com>
2021-09-01 11:23:02 -0400

    init commit

---
+++ src/main/java/com/deployment/deploymentpoc/DeploymentPocApplication.java
@@ -0,0 +1,13 @@
+package com.deployment.deploymentpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploymentPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deployment/deploymentpoc/DeploymentPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deployment.deploymentpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
